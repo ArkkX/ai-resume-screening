d99759be7b57411a003f99040e9e9d4b39144fb8
Arkk <Aasritdurbha@gmail.com>
2025-01-17 13:34:50 +0000

    git

--- ai-resume-screening/resumes/urls.py
+++
@@ -1,8 +0,0 @@
-# resumes/urls.py
-from django.urls import path
-from .views import ResumeUploadView, JobMatchView
-
-urlpatterns = [
-    path('upload/', ResumeUploadView.as_view(), name='upload'),
-    path('match/<int:candidate_id>/', JobMatchView.as_view(), name='match'),
-]
